7f2a094c8a608c8d21db634dbf5d68379cd64fa2
therajat08 <therajat08@users.noreply.github.com>
2019-07-13 13:59:39 +0530

    started repeated string problem
    so far main function operating well

---
+++ Repeated String/repeatedString.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the repeatedString function below.
+long repeatedString(string s, long n) {
+
+
+
+}
+
+int main()
+{
+    string s;
+    long n;
+    cin >>  s;
+    cin >> n;
+
+    repeatedString(s,n);
+    return 0;
+}
