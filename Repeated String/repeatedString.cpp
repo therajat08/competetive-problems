88b00b154b02112720d02d8c5c0219953f0b73f8
therajat08 <therajat08@users.noreply.github.com>
2019-07-13 15:04:37 +0530

    some test cases working

--- Repeated String/repeatedString.cpp
+++ Repeated String/repeatedString.cpp
@@ -4,8 +4,35 @@ using namespace std;
 
 // Complete the repeatedString function below.
 long repeatedString(string s, long n) {
+  int stringLength = s.length();
+    //printf("%d",stringLength);
+    long i,characterCount=0,aCount=0;
 
+    for(i=0;
+        characterCount<=n;
+        )
+        {
+            if(s[i]=='a' && s.length()==1)
+            {
+                aCount=n;
+                return aCount;
+            }
+            characterCount+=1;
+            if(s[i]=='a')
+            {
+                aCount+=1;
 
+            }
+            //printf("\n\n caharacter  %c",s[i]);
+            i++;
+
+            if((i)/stringLength==1 && (i)%stringLength==0)
+                {
+                    i=0;
+                }
+            //printf("\n\n%ld ",aCount);
+        }
+    return aCount;
 
 }
 
@@ -14,8 +41,8 @@ int main()
     string s;
     long n;
     cin >>  s;
-    cin >> n;
+    cin >>  n;
 
-    repeatedString(s,n);
+    printf("%d",repeatedString(s,n));
     return 0;
 }
