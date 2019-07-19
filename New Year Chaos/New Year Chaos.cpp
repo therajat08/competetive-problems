dbd67c8bf27f41a4aeb531fd8c144775714966eb
therajat08 <therajat08@users.noreply.github.com>
2019-07-19 18:22:50 +0530

    created main function in code blocks for my purpose; testing if main is passing queue or not

--- New Year Chaos/New Year Chaos.cpp
+++ New Year Chaos/New Year Chaos.cpp
@@ -2,70 +2,62 @@
 
 using namespace std;
 
-vector<string> split_string(string);
+
 
 // Complete the minimumBribes function below.
 void minimumBribes(vector<int> q) {
 
+    int i;
+    int bribeCount=0;
 
+    for(i=0;i<q.size();i++)
+    {
+        cout << q.at(i);
+        printf("\n");
+    }
+    printf("******end of a queue************");
+
+   /* for(i=0;i<q.size();i++)
+    {
+
+        if((q[i] - (i+1)) > 0)//testing if person is getting ahead in line(+ve difference)
+        {
+            if((q[i] - (i+1)) > 2)//testing if it is more than 2 i.e chaotic
+            {
+                printf("Too chaotic");
+            }
+            else
+            {
+                bribeCount+= (q[i] - (i+1));
+            }
+        }
+    }
+    printf("%d",bribeCount);
+*/
 }
 
 int main()
 {
-    int t;
-    cin >> t;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int numberOfQueries=0;
+    int vectSize=0;
+    int j,i,k;
+    vector<int> vect;
 
-    for (int t_itr = 0; t_itr < t; t_itr++) {
-        int n;
-        cin >> n;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin >> numberOfQueries;
 
-        string q_temp_temp;
-        getline(cin, q_temp_temp);
-
-        vector<string> q_temp = split_string(q_temp_temp);
-
-        vector<int> q(n);
-
-        for (int i = 0; i < n; i++) {
-            int q_item = stoi(q_temp[i]);
-
-            q[i] = q_item;
-        }
-
-        minimumBribes(q);
+    for(i=0;i<numberOfQueries;i++)
+    {
+        cin >> vectSize;
+            for(j=0;j<vectSize;j++)
+            {
+               cin >> k;
+                vect.push_back(k);
+            }
+        minimumBribes(vect);
+        vect.clear();//to reset out vector containing queue sequence
     }
+
 
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
 
