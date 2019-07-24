d2a789d782b21a32ffeb6a803e1a6103ebf34998
therajat08 <therajat08@users.noreply.github.com>
2019-07-24 17:34:25 +0530

    note:
    i think i am getting a segmentation fault because it is not creating as many rows as needed

--- Dynamic Array/Dynamic Array2.cpp
+++ Dynamic Array/Dynamic Array2.cpp
@@ -23,9 +23,9 @@ int main(){
         cin >> typeOfQuerry;
         cin >> x;
         cin >> y;
-        //printf("here 3********");
-        //printf("%d",(x^lastAnswer)%numberOfSequences);
-        //printf("here 4********");
+        printf("here 3********\n");
+        printf("index: %d\n",(x^lastAnswer)%numberOfSequences);
+        printf("here 4********\n");
         index = (x^lastAnswer)%numberOfSequences;
         it = find (rowCreated.begin(), rowCreated.end(), index);
         if (it == rowCreated.end())
@@ -33,36 +33,17 @@ int main(){
             vect.push_back(vector<int>());
        }
 
-        //printf("here 5********");
-        /*if(index == 0 && rowCreated>0)
-        {
-            if(flag!=1)
-                {
+        printf("here 5********\n");
 
-                    flag = 1;
-                    vect.push_back(vector<int>());
-                    rowCreated--;
-                }
-
-        }
-        else if(index == 1 && rowCreated>0)
-        {
-            if(flag!=0)
-            {
-                flag = 0;
-                vect.push_back(vector<int>());
-                rowCreated--;
-            }
-        }*/
 
 
 
         if(typeOfQuerry==1)
         {
 
-            //printf("here 1********");
+            printf("here 1********\n");
             vect[index].push_back(y);
-            //printf("here 2********");
+            printf("here 2********\n");
         }
         else if(typeOfQuerry==2)
         {
