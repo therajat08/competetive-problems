53170f792dff48664463058fefb957b38c0d80f6
therajat08 <therajat08@users.noreply.github.com>
2020-02-20 12:44:14 +0530

    more probems solved

---
+++ Algorithms/Greedy Florist/Greedy Florist.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        int numberOfFlowers;
+        int people;
+        int i,j;
+        vector<int> cost;
+        cin >> numberOfFlowers;
+        cin >> peopl;
+
+        for(i=0;i<numberOfFlowers;i++)
+        {
+
+                cin >> j;
+                cost.push_back(j);
+        }
+
+
+        return 0;
+}
+
+
+
