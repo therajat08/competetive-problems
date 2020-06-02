04e1d1fc8a12fbb962845153e44efb7349775690
Rajat Negi <therajat08@users.noreply.github.com>
2020-06-02 17:26:38 +0530

    included scan, libraries

--- templates/1/template.cpp
+++ templates/1/template.cpp
@@ -1,7 +1,28 @@
-#include <bits/stdc++.h>
-#define veci vector<int>
-#define loop(a,b) for (int i = a; i < b; i++)
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i,a,b) for (int i = a; i < b; i++)
+
 #define printVeci(v) for(int i = 0; i < v.size(); i++) {cout << v[i] << endl;} 
+
+#define si(a) scanf("%d", &a)
+#define sii(a,b) scanf("%d %d", &a, &b)
+#define siii(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a,b) scanf("%lld %lld", &a, &b)
+
+typedef vi vector<int>;
+
 using namespace std;
 
 int main()
