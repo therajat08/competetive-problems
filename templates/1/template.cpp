bbd7dd2ab1c4b6c60ea3f773a667526789074a61
Rajat Negi <therajat08@users.noreply.github.com>
2020-06-02 08:33:55 +0530

    initial template commit

---
+++ templates/1/template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define veci vector<int>
+#define loop(a,b) for (int i = a; i < b; i++)
+#define printVeci(v) for(int i = 0; i < v.size(); i++) {cout << v[i] << endl;} 
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
