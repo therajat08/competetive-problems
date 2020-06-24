00402b8ce01833e9c5f911fcb02bac9a53212ff8
therajat08 <rajatnegi13@gmail.com>
2020-06-24 14:51:04 +0530

    was accepted and is simple

---
+++ Codeforces/Contests/652 div2/A. FashionabLee/main.cpp
@@ -0,0 +1,47 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i,a,b) for (int i = a; i < b; i++)
+
+#define printVeci(v) for(int i = 0; i < v.size(); i++) {cout << v[i] << endl;} 
+
+#define si(a) scanf("%d", &a)
+#define sii(a,b) scanf("%d %d", &a, &b)
+#define siii(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a,b) scanf("%lld %lld", &a, &b)
+
+
+
+using namespace std;
+
+typedef vector<int> vi;
+
+int main()
+{
+    long long t;
+
+	sl(t);
+	long long side;
+	while(t--)
+	{
+		sl(side);
+		if (side%4==0) {
+			cout << "YES"<< endl;
+		}
+		else{
+				cout << "NO"<< endl;
+		}
+	}
+    return 0;
+}
