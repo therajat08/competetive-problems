aaff021e11e363e9e8f2c7f79467bed539df8ca0
therajat08 <rajatnegi13@gmail.com>
2020-07-05 16:35:59 +0530

    solution understood by seeing editorial

---
+++ Codeforces/Contests/654_div_2/B/Magical_Calendar.cpp
@@ -0,0 +1,70 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+	int cases;
+
+	si(cases);
+
+	while(cases--)
+	{
+		ll n,r;
+		sll(n,r);
+		ll res=0;
+		if(n<=1)
+		{
+			printf("1\n");
+			continue;
+		}
+		if(n<=r)
+		{
+			r=n;
+			res = 1 + ((r-1)*r)/2;
+		}
+		else if(n>r)
+		{
+			res = (r*(r+1))/2;
+		}
+		printf("%lld\n",res );
+	}
+    return 0;
+}
