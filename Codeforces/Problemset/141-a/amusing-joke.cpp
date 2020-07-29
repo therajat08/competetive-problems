9ecc720fcdb302d660bd377c6b1653b87dca1498
therajat08 <rajatnegi13@gmail.com>
2020-07-29 14:36:55 +0530

    a20j prob

---
+++ Codeforces/Problemset/141-a/amusing-joke.cpp
@@ -0,0 +1,60 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+   string str, str2, str3;
+   cin >> str >> str2 >> str3;
+
+   str+=str2;
+   sort(str.begin(), str.end());
+   sort(str3.begin(), str3.end());
+
+   if(str==str3)
+   {
+     printf("\nYES");
+   }
+   else{
+     printf("\nNO");
+   }
+  
+    return 0;
+}
