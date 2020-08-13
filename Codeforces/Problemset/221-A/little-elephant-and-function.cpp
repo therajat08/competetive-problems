d2d96b197b84bbce79bcb927461e88f4ff1b5050
therajat08 <rajatnegi13@gmail.com>
2020-08-13 12:10:36 +0530

    a2oj 221-A

---
+++ Codeforces/Problemset/221-A/little-elephant-and-function.cpp
@@ -0,0 +1,53 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+  int num;
+
+  cin >> num;
+  cout << num <<" ";
+  for(int i=1; i<=num-1;i++)
+  {
+    cout<< i << " ";
+  }
+    return 0;
+}
