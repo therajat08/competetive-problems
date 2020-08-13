c65664dd0e0e5466ff7eb171b877cba122f8a5e0
therajat08 <rajatnegi13@gmail.com>
2020-08-13 17:46:51 +0530

    a2oj 320-A saw solution

---
+++ Codeforces/Problemset/320-A/magic-numbers.cpp
@@ -0,0 +1,68 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+    string num;
+    cin >> num;
+    // using iterator for string traversal
+    for(int i=0;i<num.length();i++)
+    {
+      if(num[i] != '1' && num[i] != '4')
+      {
+        cout << "NO";
+        return 0;
+      }
+    }
+    if(num[0]=='4')
+    {
+        cout << "NO";
+        return 0;
+    }
+    if(num.find("444")!=num.npos)
+    {
+      cout << "NO";
+      return 0;
+    }
+
+    cout << "YES";
+    return 0;
+}
