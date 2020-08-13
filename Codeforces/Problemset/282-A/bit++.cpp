06144f5b84c671e9724fa38b23d83b937901b465
therajat08 <rajatnegi13@gmail.com>
2020-08-13 11:35:19 +0530

    a2oj A

---
+++ Codeforces/Problemset/282-A/bit++.cpp
@@ -0,0 +1,63 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+    int x=0;
+
+    int cases;
+    cin >> cases;
+    string op;    
+    while(cases--)
+    {
+      cin >> op;
+      if(op=="++X"|| op=="X++")
+      {
+        x++;
+      }
+      else
+      {
+        x--;
+      }
+    }
+    cout << x;
+    return 0;
+}
