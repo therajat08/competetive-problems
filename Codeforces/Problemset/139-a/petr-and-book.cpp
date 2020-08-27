83191bfd43777a0b997185e418b3e6a34186a2ec
therajat08 <rajatnegi13@gmail.com>
2020-08-27 10:53:07 +0530

    a2oj 139-a

---
+++ Codeforces/Problemset/139-a/petr-and-book.cpp
@@ -0,0 +1,73 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+  int pages;
+
+  cin >> pages;
+
+  int dayCapacity[7];
+  for(int i=0; i< 7; i++)
+  {
+    cin >> dayCapacity[i];
+  }
+
+  int pagesRead=0;
+
+  for(int i=0; i<7;)
+  {
+    pagesRead+=dayCapacity[i];
+    if(pagesRead>=pages)
+    {
+      cout << i+1; 
+      break;
+    }
+    if(i==6 && pagesRead<pages)
+    {
+      i = 0;
+    }
+    else{
+      i++;
+    }
+  }
+    return 0;
+}
