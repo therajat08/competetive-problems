c4910ea0e86ef5fa860b608b0561a829fcac1c10
therajat08 <rajatnegi13@gmail.com>
2020-08-28 13:34:24 +0530

    a2oj a 205-a

---
+++ Codeforces/Problemset/205-a/little-elephant-ans-rozdil.cpp
@@ -0,0 +1,71 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#define loop(i, a, b) for (int i = a; i < b; i++)
+
+#define printVeci(v)                   \
+    for (int i = 0; i < v.size(); i++) \
+    {                                  \
+        cout << v[i] << endl;          \
+    }
+#define printAri(v, s)          \
+    for (int i = 0; i < s; i++) \
+    {                           \
+        cout << v[i] << endl;   \
+    }
+#define co(a) cout << a << endl;
+#define coo(a, b) cout << a << " " << b << " " << endl;
+
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d %d", &a, &b)
+#define siii(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define siiii(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d);
+
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld %lld", &a, &b)
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long  ll;
+
+int main()
+{
+  int n;
+
+  cin >> n;
+
+  int ar[n];
+
+  for(int i=0; i< n; i++)
+  {
+    cin >> ar[i];
+  }
+
+  // sort(ar, ar+n);
+
+  int *nearest; 
+  nearest= min_element(ar, ar+n);
+  int cnt = count(ar, ar+n, *nearest);
+
+  if(cnt == 1)
+  {
+    auto itr = find(ar, ar+n, *nearest);
+    cout << distance(ar,itr) + 1;
+  }
+  else if(cnt>1)
+  {
+    cout << "Still Rozdil";
+  }
+    return 0;
+}
